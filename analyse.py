84445015b835772ae29ca7089046209afba43977
NNathanson <nathansonnina@gmail.com>
2022-07-20 15:21:18 +0200

    Added dependencies in requirements. Added the neccessary venv setup files.

---
+++ analyse.py
@@ -0,0 +1,21 @@
+from ast import Try
+from socket import socket
+import zmq
+
+context = zmq.Context()
+daq = context.socket(zmq.REP)
+daq.bind('tcp://*:7770')
+print('Waiting for wake-up command ...\n')
+data  =daq.recv_string()
+print(data)
+
+daq.send_string('Ready to analyse')
+
+
+
+
+
+
+
+   
+
